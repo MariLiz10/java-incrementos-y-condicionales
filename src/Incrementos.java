fb2f296a031fdd1e6350b77db237dafe61482a95
MariLiz10 <u20261242140@usco.edu.co>
2026-08-27 19:36:15 -0500

    Add files via upload

---
+++ src/Incrementos.java
@@ -0,0 +1,58 @@
+/*
+
+package incrementos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Incrementos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+
+ int a = 5;
+int b = ++a;
+
+int c = 5;
+ int d = c++;
+
+ System.out.println("a = " + a);
+ System.out.println("b = " + b);
+ System.out.println("c = " + c);
+ System.out.println("d = " + d);
+ 
+ // 1. Incremento en una asignación
+        int x = 10;
+        int y = ++x;
+
+        System.out.println("=== ASIGNACIÓN ===");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+
+        // 2. Incremento en una operación matemática
+        
+        int resultado = a++ + 3;
+
+        System.out.println("\n=== OPERACIÓN MATEMÁTICA ===");
+        System.out.println("resultado = " + resultado);
+        System.out.println("a = " + a);
+
+
+        // 3. Incremento dentro de un ciclo for
+        System.out.println("\n=== CICLO FOR ===");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Número: " + i);
+        }
+ 
+ 
+ 
+ 
+    }
+    
+}
